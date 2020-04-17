0e20083115ac0bb0130494ad915810071da1bad5
mvximenko <mr.mxmnk@gmail.com>
2020-04-17 23:50:26 +0900

    Start styling PaletteList, Finish styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,43 +2,63 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-    root: {
-      background: 'white',
-      border: '1px solid black',
-      borderRadius: '5px',
-      padding: '0.5rem',
-      position: 'relative',
-      overflow: 'hidden',
-      '&:hover': {
-          cursor: 'pointer'
-        }
-    },
-    colors: {
-      backgroundColor: 'grey'
-    },
-    title: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      margin: '0',
-      color: 'black',
-      paddingTop: '0.5rem',
-      fontSize: '1rem',
-      position: 'relative'
-    },
-    emoji: {
-      marginLeft: '0.5rem',
-      fontSize: '1.5rem'
+  root: {
+    backgroundColor: 'white',
+    border: '1px solid black',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer'
     }
+  },
+  colors: {
+    backgroundColor: '#dae1e4',
+    height: '150px',
+    width: '100%',
+    borderRadius: '5px',
+    overflow: 'hidden'
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative'
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem'
+  },
+  miniColor: {
+    height: '25%',
+    width: '20%',
+    display: 'inline-block',
+    margin: '0 auto',
+    position: 'relative',
+    marginBottom: '-4px'
+  }
 };
 
 function MiniPalette(props) {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map(color => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    />
+  ));
+
   return (
     <div className={classes.root}>
-      <div className={classes.colors}></div>
-      <h5 className={classes.title}>{paletteName} 
-        <span className={classes.emoji}>{emoji}</span>
+      <div className={classes.colors}>{miniColorBoxes}</div>
+      <h5 className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
       </h5>
     </div>
   );
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+  root: {
+    backgroundColor: 'blue',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+  container: {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    flexWrap: 'wrap'
+  },
+  nav: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between'
+  },
+  palettes: {
+    boxSizing: 'border-box',
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '5%'
+  }
+};
 
 class PaletteList extends Component {
   render() {
-    const { palettes } = this.props;
+    const { palettes, classes } = this.props;
     return (
-      <div>
-        <MiniPalette />
-        <h1> React Colors </h1>
-        {palettes.map(palette => (
-            <MiniPalette {...palette}/>
-        ))}
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React Colors</h1>
+          </nav>
+          <div className={classes.palettes}>
+            {palettes.map(palette => (
+              <MiniPalette {...palette} />
+            ))}
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
