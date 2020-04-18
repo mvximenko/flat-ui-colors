8f2650044bbfd16e11684a8574c3a6f8e8b2291b
mvximenko <mr.mxmnk@gmail.com>
2020-04-18 13:25:56 +0900

    Add Link to ColorBox and Add Route

--- src/App.js
+++ src/App.js
@@ -33,6 +33,11 @@ class App extends Component {
             />
           )} 
         />
+        <Route 
+          exact 
+          path='/palette/:paletteId/:colorId' 
+          render={() => <h1>Single Color Page!</h1>} 
+        />
       </Switch>
   //    <div>
   //     <Palette palette={generatePalette(seedColors[4])}_ />
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Link } from 'react-router-dom';
 import './ColorBox.css';
 
 class ColorBox extends Component {
@@ -33,7 +34,9 @@ class ColorBox extends Component {
             </div>
             <button className='copy-button'>Copy</button>
           </div>
-          <span className='see-more'>More</span>
+          <Link to='/' onClick={e => e.stopPropagation()}>
+            <span className='see-more'>More</span>
+          </Link>
         </div>
       </CopyToClipboard>
     )
