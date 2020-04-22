b57ede769278f80a81d3303ae3a6e151d23f5b0b
mvximenko <mr.mxmnk@gmail.com>
2020-04-22 22:14:50 +0900

    Add SavePalette

--- src/App.js
+++ src/App.js
@@ -9,10 +9,19 @@ import { generatePalette } from './colorHelpers'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { palettes: seedColors };
+    this.savePalette = this.savePalette.bind(this);
+    this.findPalette = this.findPalette.bind(this);
+  }
   findPalette(id) {
-    return seedColors.find(function(palette) {
+    return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
+  }
+  savePalette(newPalette) {
+    this.setState({ palettes: [...this.state.palettes, newPalette] });
   }
   render() {
     return (
@@ -20,7 +29,9 @@ class App extends Component {
         <Route 
           exact
           path='/palette/new'
-          render={() => <NewPaletteForm />} 
+          render={(routeProps) => (
+            <NewPaletteForm savePalette={this.savePalette} {...routeProps} />
+          )} 
          />
         <Route 
           exact 
@@ -38,7 +49,7 @@ class App extends Component {
           exact 
           path='/' 
           render={routeProps => (
-            <PaletteList palettes={seedColors} {...routeProps} />
+            <PaletteList palettes={this.state.palettes} {...routeProps} />
           )} 
         />
         <Route 
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -87,6 +87,7 @@ class NewPaletteForm extends Component {
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.addNewColor = this.addNewColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
     ValidatorForm.addValidationRule('isColorNameUnique', value =>
@@ -121,7 +122,16 @@ class NewPaletteForm extends Component {
   handleChange(evt) {
     this.setState({ newName: evt.target.value });
   }
-
+  handleSubmit() {
+  	let newName = 'New Test Palette';
+  	const newPalette = {
+  		paletteName: newName,
+  		id: newName.toLowerCase().replace(/ /g, "-"),
+  		colors: this.state.colors
+  	};
+  	this.props.savePalette(newPalette);
+  	this.props.history.push('/');
+  }
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -131,6 +141,7 @@ class NewPaletteForm extends Component {
         <CssBaseline />
         <AppBar
           position='fixed'
+          color='default'
           className={classNames(classes.appBar, {
             [classes.appBarShift]: open
           })}
@@ -147,6 +158,13 @@ class NewPaletteForm extends Component {
             <Typography variant='h6' color='inherit' noWrap>
               Persistent drawer
             </Typography>
+            <Button 
+            	variant='contained' 
+            	color='primary'
+            	onClick={this.handleSubmit}
+            >
+            	Save Palette
+            </Button>
           </Toolbar>
         </AppBar>
         <Drawer
