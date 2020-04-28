37b6694ff0732ab5ac484364db6ea2d168fe4b8d
mvximenko <mr.mxmnk@gmail.com>
2020-04-28 16:01:07 +0900

    Animate palette deletion

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { withStyles } from '@material-ui/styles';
 import styles from './styles/PaletteListStyles';
-import MiniPalette from './MiniPalette';
 
 class PaletteList extends Component {
   goToPalette(id) {
@@ -17,17 +18,19 @@ class PaletteList extends Component {
             <h1 className={classes.heading}>REACT COLORS</h1>
             <Link to='/palette/new'>Create Palette</Link>
           </nav>
-          <div className={classes.palettes}>
+          <TransitionGroup className={classes.palettes}>
             {palettes.map((palette) => (
-              <MiniPalette
-                {...palette}
-                handleClick={() => this.goToPalette(palette.id)}
-                handleDelete={deletePalette}
-                key={palette.id}
-                id={palette.id}
-              />
+              <CSSTransition key={palette.id} classNames='fade' timeout={500}>
+                <MiniPalette
+                  {...palette}
+                  handleClick={() => this.goToPalette(palette.id)}
+                  handleDelete={deletePalette}
+                  key={palette.id}
+                  id={palette.id}
+                />
+              </CSSTransition>
             ))}
-          </div>
+          </TransitionGroup>
         </div>
       </div>
     );
--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,6 +1,15 @@
 import sizes from './sizes';
 import bg from './bg.svg';
 export default {
+  '@global': {
+    '.fade-exit': {
+      opacity: 1,
+    },
+    '.fade-exit-active': {
+      opacity: 0,
+      transition: 'opacity 500ms ease-out',
+    },
+  },
   root: {
     backgroundColor: 'blue',
     height: '100vh',
